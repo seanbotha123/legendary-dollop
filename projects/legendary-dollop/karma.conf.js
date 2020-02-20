905941cedad64facf98a4d3a3e123c9d09e4fe29
Sean Botha <seanbotha.1@gmail.com>
2020-02-20 22:54:49 +0200

    update thresholds

--- projects/legendary-dollop/karma.conf.js
+++ projects/legendary-dollop/karma.conf.js
@@ -1,49 +1,4 @@
-// // Karma configuration file, see link for more information
-// // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath() //Used for headless support
-// module.exports = function (config) {
-//     config.set({
-//         basePath: '',
-//         frameworks: ['jasmine', '@angular-devkit/build-angular'],
-//         plugins: [
-//             require('karma-jasmine'),
-//             require('karma-chrome-launcher'),
-//             require('karma-jasmine-html-reporter'),
-//             require('karma-coverage-istanbul-reporter'),
-//             require('@angular-devkit/build-angular/plugins/karma')
-//         ],
-//         client: {
-//             clearContext: false // leave Jasmine Spec Runner output visible in browser
-//         },
-//         coverageIstanbulReporter: {
-//             skipFilesWithNoCoverage: false,
-//             includeAllSources: true,
-//             dir: require('path').join(__dirname, '../../coverage/legendary-dollop'),
-//             reports: ['html', 'lcovonly', 'text-summary'],
-//             fixWebpackSourcePaths: true,
-//             thresholds: {
-//                 statements: 100,
-//                 lines: 100,
-//                 branches: 100,
-//                 functions: 100
-//             }
-//         },
-//         reporters: ['progress', 'kjhtml'],
-//         port: 9876,
-//         colors: true,
-//         logLevel: config.LOG_INFO,
-//         autoWatch: true,
-//         browsers: ['ChromeHeadlessNoSandbox'],
-//         customLaunchers: {
-//             ChromeHeadlessNoSandbox: {
-//                 base: 'ChromeHeadless',
-//                 flags: ['--no-sandbox']
-//             }
-//         },
-//         singleRun: true
-//     });
-// };
-
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
     config.set({
         basePath: '',
@@ -56,16 +11,16 @@ module.exports = function (config) {
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
+            clearContext: false
         },
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, '../../coverage/legendary-dollop'),
             reports: ['lcovonly'],
             thresholds: {
-                statements: 100,
-                lines: 100,
-                branches: 100,
-                functions: 100
+                statements: 80,
+                lines: 80,
+                branches: 80,
+                functions: 80
             },
             fixWebpackSourcePaths: true
         },
